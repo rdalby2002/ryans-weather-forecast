fd789ed062138dea945543027067d3f05f9d0476
rdalby2002 <ryancdalby@gmail.com>
2022-11-21 15:35:09 -0600

    Fixed a fetch bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ var cityInputEL = $('#cityinput');
 var searchButtonEL = $('#srchbtn');
 var searchHistoryEL = $('#searchhistory');
 var currentWeatherEL = $('#currentweather');
-var forecastEL = $('forecast');
+var forecastEL = $('#forecast');
 var APIkey = '4e03668922cf56c28728986d0737b89e';
 
 var getCurrentWeather = (event) => {
-    var city = cityInputEL.val();
-    var coordQueryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIkey;
+    var cityInput = cityInputEL.val();
+    var coordQueryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&limit=1&appid=' + APIkey;
     fetch(coordQueryURL)
     .then((response) => {
         return response.json();
@@ -15,24 +15,25 @@ var getCurrentWeather = (event) => {
     .then((data) => {
         var lat = data[0].lat;
         var lon = data[0].lon;
-        var forecastQueryURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + APIkey;
+        var forecastQueryURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + APIkey;
         fetch(forecastQueryURL)
         .then((response) => {
             return response.json();
         })
-        .then((response) => {
-            var currentWeatherIcon = 'https://openweathermap.org/img/w/' + response.weather[0].icon + '.png';
-            var currentTimeUTC = response.dt;
-            var currentTimeZoneOffset = response.timezone;
+        .then((data) => {
+            var currentWeatherIcon = 'http://openweathermap.org/img/w/' + data.list[0].weather.icon + '.png';
+            var currentTimeUTC = data.list[0].dt;
+            var currentTimeZoneOffset = data.city.timezone;
             var currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 /60;
             var currentTime = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
 
+            currentWeatherEL.addClass('border border-1 border-dark')
             var currentWeather = `
-            <h3>${response.name} ${currentTime.format("MM/DD/YYY")}<img src="${currentWeatherIcon}"/></h3>
+            <h3>${data.city.name} ${currentTime.format("MM/DD/YYYY")} <img src="${currentWeatherIcon}"/></h3>
             <ul class="list-unstyled">
-                <li>Temp: ${response.main.temp} °F;</li>
-                <li>Wind: ${response.wind.speed} MPH</li>
-                <li>Humidity: ${response.main.humidity}%</li>
+                <li class="py-2">Temp: ${data.list[0].main.temp} °F</li>
+                <li class="py-2">Wind: ${data.list[0].wind.speed} MPH</li>
+                <li class="py-2">Humidity: ${data.list[0].main.humidity}%</li>
             </ul>`;
             currentWeatherEL.html(currentWeather);
         })
